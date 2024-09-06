efe4533cc1bc275fa5d25571da4cc5e53c3f6a30
aleksmn <al-mn@yandex.ru>
2024-09-06 11:39:33 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pixel Adventure Python Platformer
+
+
+Assets Source
+https://pixelfrog-assets.itch.io/pixel-adventure-1
---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import random
+import math
+import pygame as pg
+from os import listdir
+from os.path import isfile, join
+
+pg.init()
+
+BG_COLOR = (255, 255, 255)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+FPS = 60
+PLAYER_VELOCITY = 5
+
+
+
+class Game:
+
+    def __init__(self):        
+        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pg.display.set_caption("Pixel Adventure")
+        
+        self.clock = pg.time.Clock()
+        
+
+        self.run()
+
+    def event(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+
+
+    def update(self):
+        pass
+
+
+    def draw(self):
+        pass
+
+
+    def run(self):
+        while True:
+            self.event()
+            self.update()
+            self.draw()
+            self.clock.tick(FPS)
+            pg.display.flip()
+
+
+# Точка входа в программу
+if __name__ == "__main__":
+    # Запускаем игру
+    Game()
